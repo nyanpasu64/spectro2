4564f7e0af87d32d5458cb3d7e1daee4ca24948a
nyanpasu64 <nyanpasu64@tuta.io>
2020-09-22 05:16:59 -0700

    clear to black

--- src/main.rs
+++ src/main.rs
@@ -424,9 +424,9 @@ impl State {
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
+                            r: 0.0,
+                            g: 0.0,
+                            b: 0.0,
                             a: 1.0,
                         }),
                         store: true,
